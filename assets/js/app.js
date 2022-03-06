d8caf9e1f7b515e362ef60f79aa6e12b64c6c863
ApolloSolo <matt1teixeira@gmail.com>
2022-03-06 18:18:10 -0500

    Inital commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-3');
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 250){
+        header.classList.add('active');
+    } else{
+        header.classList.remove('active');
+    }
+}
+
+var swiper = new Swiper(".home-slider", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+  });
